d5f7343ac6f006d5ce080cecce916567d8b89651
AuroraPetard <lzq@192.168.1.9>
2023-05-09 00:01:24 +0800

    fix

--- weatherForecast_g1.py
+++ weatherForecast_g1.py
@@ -38,7 +38,7 @@ def get_weather(city):
     return res
 
 
-week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+week_list = [" 星期一", " 星期二", " 星期三", " 星期四", " 星期五", " 星期六", " 星期日"]
 
 
 def get_week(dateTime):
